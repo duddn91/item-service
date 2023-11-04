86c70095d01bdaf2fc1c8214c1f7cc4b34b3c4e9
byun <rpdla88203@gmail.com>
2023-11-04 23:41:01 +0900

    타임리프를 사용한 셀렉트박스

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,7 +1,8 @@
 package hello.itemservice.domain.item;
 
-public enum ItemType {
+import lombok.Getter;
 
+public enum ItemType {
     BOOK("도서"), FOOD("음식"), ETC("기타");
 
     private final String description;
